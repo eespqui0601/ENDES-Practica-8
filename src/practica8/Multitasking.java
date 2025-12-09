be810dd3eb8ece662b46367ac839ed1fd00e8788
eespqui0601 <eespqui0601@g.educaand.es>
2025-12-09 20:17:16 +0100

    Commit inicial

---
+++ src/practica8/Multitasking.java
@@ -0,0 +1,33 @@
+package practica8;
+
+import java.util.Scanner;
+
+public class Multitasking {
+	public void multitasking() {
+		
+		Scanner keyboard = new Scanner (System.in);
+		
+		System.out.printf("Introduzca: \n 1- Invertir palabra \n 2- Realizar dibujo \n 3- Hacer cálculo \n");
+		int n = keyboard.nextInt();
+		
+		if (n == 1) {
+			
+		}
+		else if (n == 2) {
+			
+		}
+		else if (n == 3) {
+			
+		}
+		else {
+			while (n > 3) {
+				System.out.println("error");
+				n = keyboard.nextInt();
+			}
+		}
+		
+	}
+public static void main (String [] args) {
+	new Multitasking().multitasking();
+	}
+}
