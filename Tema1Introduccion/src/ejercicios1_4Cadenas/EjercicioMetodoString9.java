a3758cd1c2e6918a2620b86b5b1dbaf0ee53ad30
cnunlop1005 <cnunlop1005@g.educaand.es>
2025-12-09 23:31:21 +0100

    Segunda version de trabajo-Cristian

--- Tema1Introduccion/src/ejercicios1_4Cadenas/EjercicioMetodoString9.java
+++ Tema1Introduccion/src/ejercicios1_4Cadenas/EjercicioMetodoString9.java
@@ -6,14 +6,17 @@ public class EjercicioMetodoString9 {
 	
 	public void ex1() {
 	
-	System.out.println("Introduzca una palabra: ");
+	System.out.println("Introduzca un texto: ");
 	Scanner keyboard = new Scanner (System.in);
 	String word = keyboard.nextLine();
 	String reverse = "";
 	for (int i = word.length() - 1; i >= 0; i--) {
         reverse += word.charAt(i);
 	}
-	System.out.println("Después de invertir: " + reverse);
+	final String RED = "\u001B[31m";
+	final String RESET = "\u001B[0m";
+
+	System.out.println("Después de invertir: " + RED + reverse + RESET);
 }
 
 	public static void main(String[] args) {
